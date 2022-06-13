fa310ffb2e3bb7b6aecf0d73942d8da6cf0e5f59
afnnnnn <103879224+afnnnnn@users.noreply.github.com>
2022-06-14 00:55:02 +0800

    Update profile.blade.php

--- profile.blade.php
+++ profile.blade.php
@@ -42,9 +42,9 @@
     $password = "password";
     $dbname = "myDB";
 
-    // Create connection
+    
     $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
+    
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
