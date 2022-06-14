dabc7c9c9af323212c69d49912fd5cee82120259
afnnnnn <103879224+afnnnnn@users.noreply.github.com>
2022-06-14 15:23:44 +0800

    Update appointment.blade.php

--- appointment.blade.php
+++ appointment.blade.php
@@ -18,7 +18,7 @@
         </style>
 
     </head>
-    <body
+    <body>
     <header>
         <a href="#" class="logo"><img src="img/hospital.png" style="width: 45px; height: 45px;"></a>
         <div class="menuToggle" onclick="toggleMenu();"></div>
@@ -36,31 +36,9 @@
             <h3 class="card-title">List of Appointments</h3>
         </div>
         <div class="Bubble">
-            <div class="container-fluid">
-                <div class="row" style="display:none" id="selected_opt">
-                    <div class="w-100 d-flex">
-                        <div class="col-2">
-                            <label for="" class="controllabel"> With Selected:</label>
-                        </div>
-                        <div class="col">
-                            <button class="btn btn-primary" type="button" id="selected_go">Go</button>
-                        </div>
-                    </div>
-                </div>
-                <table class="table table-bordered table-stripped" id="indi-list">
-                    <colgroup>
-                        <col width="5%">
-                        <col width="5%">
-                        <col width="25%">
-                        <col width="25%">
-                        <col width="20%">
-                        <col width="20%">
-                    </colgroup>
+                <table class="table">
                     <thead>
                         <tr>
-                            <td class="text-center"><div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="selectAll">
-                                </div></td>
                             <th class="text-center">#</th>
                             <th>Appointment no.</th>
                             <th>Patient ID</th>
@@ -86,17 +64,6 @@
                                 <td><?php echo $row['patient_id'] ?></td>
                                 <td><?php echo date("M d,Y h:i A",strtotime($row['appt_date'])) ?></td>
                                 <td><?php echo $row['appt_time'] ?></td>
-                                <td align="center">
-                                     <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-                                              Action
-                                        <span class="sr-only">Toggle Dropdown</span>
-                                      </button>
-                                      <div class="dropdown-menu" role="menu">
-                                        <a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['aid'] ?>"> View</a>
-                                        <div class="divider"></div>
-                                        <a class="dropdown-item edit_data" href="javascript:void(0)" data-id="<?php echo $row['aid'] ?>"> Edit</a>
-                                      </div>
-                                </td>
                             </tr>
                    
                     </tbody>
@@ -119,8 +86,7 @@
                 }
 
                 $conn->close();
-                ?>
-            </div>
+                ?>           
             </div>
         </div>
     </div>
